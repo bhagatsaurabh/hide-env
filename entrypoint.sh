380bea11736da309f7052ced673b04376125e4ea
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-06-19 01:47:05 +0530

    feat: dev de-provisioning

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+mkdir -p /home/devuser/.ssh
+
+if [ -f /devconfig/authorized_keys ]; then
+  cp /devconfig/authorized_keys /home/devuser/.ssh/authorized_keys
+elif [ -n "$SSH_PUBLIC_KEY" ]; then
+  echo "$SSH_PUBLIC_KEY" >/home/devuser/.ssh/authorized_keys
+  echo "$SSH_PUBLIC_KEY" >/devconfig/authorized_keys
+else
+  echo "[WARN] No SSH key provided"
+fi
+
+chown -R devuser:devuser /home/devuser/.ssh
+chmod 600 /home/devuser/.ssh/authorized_keys
+passwd -u devuser
+/usr/bin/supervisord -c /etc/supervisord.conf
--- filesystem/server/server.go
+++ filesystem/server/server.go
@@ -13,6 +13,9 @@ type Server struct {
 
 func NewServer() *Server {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 	mux.HandleFunc("/api/stat", handlers.StatHandler)
 	mux.HandleFunc("/api/dir", handlers.DirHandler)
 	mux.HandleFunc("/api/hash", handlers.HashHandler)
